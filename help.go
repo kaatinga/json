8246e9e63f18d4da761328c58952637f3f4daf99
Michael Gunkoff <m.onishenko@qvant.ru>
2021-04-02 18:43:06 +0300

    some consts were added

--- help.go
+++ help.go
@@ -1,1 +1,36 @@
 package json
+
+const (
+	maximumSampleLength = 65535
+	minimumJSONLength   = 2
+
+	// samples
+	//  { Object start
+	//  [ Array start
+	//  } Object end
+	//  ] Array End
+	//  , Literal comma
+	//  : Literal colon
+	//  t JSON true
+	//  f JSON false
+	//  n JSON null
+	//  " A string, possibly containing backslash escaped entities.
+	//  -, 0-9 A number
+
+	ObjectStart   byte = '{'
+	ObjectEnd     byte = '}'
+	Colon         byte = ':'
+	Comma         byte = ','
+	ArrayStart    byte = '['
+	ArrayEnd      byte = ']'
+	QuotationMark byte = '"'
+	True          byte = 't'
+	False         byte = 'f'
+	Null          byte = 'n'
+)
+
+var (
+	jsonTrue  = []byte("true")
+	jsonFalse = []byte("false")
+	jsonNull  = []byte("null")
+)
