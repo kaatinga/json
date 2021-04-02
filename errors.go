e4e59ab9b290c7299ea929e4dd8c06a751596a62
Michael Gunkoff <m.onishenko@qvant.ru>
2021-04-02 18:43:17 +0300

    some const errors were added

--- errors.go
+++ errors.go
@@ -1,1 +1,14 @@
 package json
+
+import cer "github.com/kaatinga/const-errs"
+
+const (
+	ErrInvalidSampleLength cer.Error = "invalid sample length"
+	ErrInvalidDataLength   cer.Error = "invalid data length"
+	ErrInvalidJSON         cer.Error = "json format is not valid"
+
+	ErrInvalidValue cer.Error = "the value was not read as data format is corrupted"
+
+	WarnNotFound     cer.Warning = "the sample was not found in the provided data"
+	WarnNullWasFound cer.Warning = "null was detected as value"
+)
